767f4b8bee32f5626e8028cd92fefefab647df37
Magomedali <web-ali@yandex.ru>
2018-12-19 00:22:59 +0300

    init

--- app/src/main/java/ru/web_ali/profilelk/MainActivity.java
+++ app/src/main/java/ru/web_ali/profilelk/MainActivity.java
@@ -1,21 +1,43 @@
 package ru.web_ali.profilelk;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String LOG_ID = "mainactivity";
+
+    Button submitBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submitBtn = (Button)findViewById(R.id.submit_btn);
+
+        View.OnClickListener submitForm = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText login = (EditText) findViewById(R.id.input_login);
+                EditText password = (EditText) findViewById(R.id.input_password);
+
+                Log.d(LOG_ID,login.getText().toString());
+                Log.d(LOG_ID,password.getText().toString());
+            }
+        };
+
+        submitBtn.setOnClickListener(submitForm);
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -23,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
